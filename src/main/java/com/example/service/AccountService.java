c24bfeec2d3b128bc64c7e8bd824f007e528b174
Zhuo Feng Situ <situzhuofeng@yahoo.com>
2025-06-20 07:37:11 +0000

    Created register method in AccountService

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,29 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    private AccountRepository accountRepository;
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+    public Account register(Account account){
+        Optional<Account> accountOptional = accountRepository.findByUsername(account.getUsername());
+        if (accountOptional.isPresent()){
+            return null;
+        }
+        else{
+            Account newAccount = accountOptional.get();
+            if ((newAccount.getUsername() != null) && (newAccount.getPassword().length() >= 4)){
+                return newAccount;
+            }
+        }
+        return null;
+    }
 }
