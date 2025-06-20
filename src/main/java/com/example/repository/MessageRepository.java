f1db34247b583a62931dc69e2f51855c9ac47167
Zhuo Feng Situ <situzhuofeng@yahoo.com>
2025-06-20 10:31:23 +0000

    Revised findMessagesByPostedBy method in MessageRepository

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.Message;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
-    List<Message> findMessagesByPostedBy(int postedBy);
+    //List<Message> findMessagesByPostedBy(int postedBy);
+    @Query("SELECT m FROM Message m WHERE m.postedBy = :postedBy")
+    List<Message> findMessagesByPostedBy(@Param("postedBy") int postedBy);
 }
 
